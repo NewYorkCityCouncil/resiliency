91c483356d415422f8ab706f7634cf057db5cbd5
Ben Witte <bwitte@council.nyc.gov>
2020-02-12 20:46:03 -0500

    updated 01 to include 2019

--- code/01_landsat_air_correlation.R
+++ code/01_landsat_air_correlation.R
@@ -30,6 +30,25 @@ library(lubridate)
 #' NYC Coordinates: 40.7128° N, 74.0060° W
 #' L8 Data User's Handbook:
 #' https://prd-wret.s3-us-west-2.amazonaws.com/assets/palladium/production/atoms/files/LSDS-1574_L8_Data_Users_Handbook-v5.0.pdf
+#' 
+#' 
+#' Note on Cloud Coverage:
+#' According to the documentation, cloud coverage levels over 65% are considered
+#' cloudy. Days with high cloud coverage seem to present clearly inaccurate 
+#' results; 9/8/14, for example, has a cloud coverage of 85% and shows Central
+#' Park as having an average temperature of -65 F.
+#' 
+#' While Earth explorer allows users to filter based on cloud coverage (and if
+#' repeating this endeavor, I'd encourage you to download from the source while
+#' using this filter), because we already downloaded all dates, I will remove
+#' days with significant cloud coverage by pulling from the xml data, which con-
+#' tains cloud cover information.
+#' 
+#' 
+#' Note on Temperatures:
+#' Temperatures are recorded by LANDSAT 8 in Kelvin*10. In order to make them
+#' readable to the general public, you'll see I've added a Kelvin to Fahrenheit
+#' converter function, and that I divide all values by 10.
 
 
 
@@ -44,14 +63,6 @@ cp_raw <- read_csv("data/input/Ground_Monitor_Temps_NYC/central_park_temp.csv") 
   dplyr::select(station, date, hourly_dry_bulb_temperature) %>% 
   mutate(name = "Central Park")
 
-#separate date and time out, as we may not be able to use time
-# hours_cp <- format(as.POSIXct(strptime(cp_raw$date,"%Y-%m-%d %H:%M",tz="")) ,format = "%H:%M")
-# 
-# dates_cp <- format(as.POSIXct(strptime(cp_raw$date,"%Y-%m-%d %H:%M",tz="")) ,format = "%Y-%m-%d")
-# 
-# cp_raw$date <- dates_cp
-# cp_raw$hour <- hours_cp
-
 cp_raw <- cp_raw %>% 
   filter(!is.na(date))
 
@@ -60,15 +71,6 @@ lag_raw <- read_csv("data/input/Ground_Monitor_Temps_NYC/laguardia_temp.csv") %>
   janitor::clean_names() %>% 
   dplyr::select(station, date, hourly_dry_bulb_temperature) %>% 
   mutate(name = "La Guardia Airport")
-
-#separate date and time out, as we may not be able to use time
-
-# hours_lag <- format(as.POSIXct(strptime(lag_raw$date,"%Y-%m-%d %H:%M",tz="")) ,format = "%H:%M")
-# 
-# dates_lag <- format(as.POSIXct(strptime(lag_raw$date,"%Y-%m-%d %H:%M",tz="")) ,format = "%Y-%m-%d")
-# 
-# lag_raw$date <- dates_lag
-# lag_raw$hour <- hours_lag
 
 lag_raw <- lag_raw %>% 
   filter(!is.na(date))
@@ -101,7 +103,7 @@ shapes <- rbind(lag_shape, cp_shape)
 
 # Landsat Temps -------------------------------------------------------------
 
-
+# Converting Kelving to Fahrenheit
 k_to_f <- function(temp) { fahrenheight <- ((temp - 273) * (9/5)) + 32  }
 
 temp_func_2 <-function(rastername) {
@@ -124,9 +126,8 @@ temp_func_2 <-function(rastername) {
 # within the sf geometry
 
 filenames <- list.files("data/input/landsat_st", pattern="*.tif", full.names=TRUE)
-test_filenames = filenames[1:3]
-ldf <- lapply(filenames, raster)
-res <- lapply(ldf, temp_func_2)
+ldf_r <- lapply(filenames, raster)
+res <- lapply(ldf_r, temp_func_2)
 
 #pull out just the date for the names of each dataframe
 dataset_names <- str_extract(str_extract(filenames, pattern ="029007_[0-9]*"), pattern = "_[0-9]*")
@@ -136,7 +137,8 @@ names(res) <- dataset_names
 
 #join listed dataframes into single dataframe
 raster_shape <- bind_rows(res, .id = "column_label") %>% 
-  mutate(date = lubridate::ymd(str_replace(raster_shape$column_label, "_", "")))
+  mutate(date = lubridate::ymd(str_replace(column_label, "_", ""))) %>% 
+  filter(!is.na(max_temp))
 
 # a lot of values are coming up as n/a - every third value, in fact.
 
@@ -144,23 +146,27 @@ raster_shape <- bind_rows(res, .id = "column_label") %>%
 # they are categorized under NYC, the satelite doesn't actually pass over much 
 # of the city, if any. But they still count it as a NYC file. These will be 
 # removed from the analysis
-
-
-
-# raster_test <- raster("data/input/landsat_st/LC08_CU_029007_20140606_20190504_C01_V01_ST.tif")
+# 
+# 
+# raster_test <- raster("data/input/landsat_st/LC08_CU_029007_20140908_20190503_C01_V01_ST.tif")
+# raster_test <- raster("data/input/landsat_st/LC08_CU_029007_20140604_20190504_C01_V01_ST.tif")
+# plot(raster_test)
 # rstr <- velox(raster_test)
 # sf <- st_transform(shapes, crs(raster_test))
 # temps <- rstr$extract(sp=sf$geometry)
-# mean_temp <- paste0('mean_temp_', stringname)
-# max_temp <- paste0('max_temp_', stringname)
-# min_temp <- paste0('min_temp_', stringname)
 # sf$mean <- sapply(temps, mean, na.rm = TRUE)
-# sf[,mean_temp] <- k_to_f(sf[,mean_temp]/10)
-# sf[,max_temp] <- sapply(temps, max)
-# sf[,max_temp] <- k_to_f(sf[,max_temp]/10)
-# sf[,min_temp] <- sapply(temps, min)
-# sf[,min_temp] <- k_to_f(sf[,min_temp]/10)
-# sf
+# sf$mean_temp <- k_to_f(sf$mean/10)
+# sf$max_temp <- sapply(temps, max)
+# sf$max_f <- k_to_f(sf$max_temp/10)
+
+
+
+
+
+
+
+# XML Cloud Cover Extraction ----------------------------------------------
+
 
 
 #' After examining the documentation and the metadata provided in the xml files,
@@ -177,15 +183,45 @@ xmldf <- lapply(xmlfilenames, xmlTreeParse)
 xmldf <- lapply(xmldf, xmlRoot)
 
 
+
 #as.POSIXct(trimws(str_replace_all(xmlValue(xmldf[[4]][["tile_metadata"]][["global_metadata"]][["aquisition"]][[1]]), "[A-Z]", " ")))
 
-# Extract all times
+# # Extract all times
+# for (i in 1:length(xmldf)) {
+#      print(str_replace_all(xmlValue(xmldf[[i]][["scene_metadata"]][["global_metadata"]][["scene_center_time"]][[1]]), "\\.[0-9A-Z]*", ""))
+# }
+
+# Extract date and cloud cover information
+cloud_list <- list()
 for (i in 1:length(xmldf)) {
-     print(str_replace_all(xmlValue(xmldf[[i]][["scene_metadata"]][["global_metadata"]][["scene_center_time"]][[1]]), "\\.[0-9A-Z]*", ""))
+  date <- xmlValue(xmldf[[i]][["tile_metadata"]][["global_metadata"]][["acquisition_date"]])
+  tmp <- list(cloud_cover = xmlValue(xmldf[[i]][["tile_metadata"]][["global_metadata"]][["cloud_cover"]]))
+  cloud_list[[date]] <- tmp
 }
 
-#date test
-xmlValue(rootnode[[1]][[1]][[9]])
+#transform to dataframe so we can join it with the raster dataframe
+cloud_data  <-  as.data.frame(x = list(
+  date = ymd(names(cloud_list)), 
+  cloud_cover = as.numeric(matrix(unlist(cloud_list)))), 
+  stringsAsFactors = FALSE)
+
+
+#' This allows us to see which raster files both cover New York effectively 
+#' (and therefore have valid raster values), and which have reasonably low 
+#' cloud coverage, which, based on a discussion with a NASA-affiliated expert, 
+#' we'll at below 10. Also based on this discussion, we'll attempt to limit
+#' what data we actually use to the last year or two, if possible.
+raster_with_metadata <- left_join(cloud_data, raster_shape) %>% 
+  filter(cloud_cover < 10,
+         !is.na(mean_temp))
+
+
+
+
+# Usage Conclusion --------------------------------------------------------
+
+#' will use 9/22/2019, 8/30/2019 and possibly 7/10/2018 for mapping. See notes
+#' at top section of 02_mapping_rasters for reasoning.
 
 
 
@@ -195,7 +231,7 @@ xmlValue(rootnode[[1]][[1]][[9]])
 
 
 ## Aggregate the air temps by day
-cp_avg <- cp_raw %>%  
+cp_dates <- cp_raw %>%  
   filter(!is.na(hourly_dry_bulb_temperature),
          format(date,format='%Y-%m-%d') %in% as.character(raster_shape$date),
          as.POSIXct(format(date,format='%H:%M:%S'), format = "%H:%M:%S")
@@ -206,22 +242,33 @@ cp_avg <- cp_raw %>%
                as.POSIXct(paste0(today("EST"), "16:00:00"), 
                           format = "%Y-%m-%d %H:%M:%S", tz = "EST")))
                                                                        
-                                                                       ),))
-  mutate(hour = chron::chron(times  = paste0(cp_raw$hour, ":00"))) %>% 
-  filter(as.character(date) %in% as.character(raster_shape$date),
-         !is.na(hourly_dry_bulb_temperature))
   
-cp_dates <- 
+cp_avg <- aggregate(list(avg_ground_temp = cp_dates$hourly_dry_bulb_temperature),
+                    by = list(date = as.POSIXct(format(cp_dates$date,format='%Y-%m-%d')),
+                              name = cp_dates$name),
+                    function(x) {round(mean(x),0)})
 
 
-cp_test_avg <- round(mean(cp_day_test$hourly_dry_bulb_temperature),0)
 
-lag_day_test <- lag_raw %>% 
-  filter(date == as.POSIXct("2014-06-04"),
-         !is.na(hourly_dry_bulb_temperature))
+lag_dates <- lag_raw %>%  
+  filter(!is.na(hourly_dry_bulb_temperature),
+         format(date,format='%Y-%m-%d') %in% as.character(raster_shape$date),
+         as.POSIXct(format(date,format='%H:%M:%S'), format = "%H:%M:%S")
+         %within%
+           interval(
+             as.POSIXct(paste0(today("EST"), "15:00:00"), 
+                        format = "%Y-%m-%d %H:%M:%S", tz = "EST"), 
+             as.POSIXct(paste0(today("EST"), "16:00:00"), 
+                        format = "%Y-%m-%d %H:%M:%S", tz = "EST")))
 
-lag_test_avg <- round(mean(lag_day_test$hourly_dry_bulb_temperature),0)
+lag_avg <- aggregate(list(avg_ground_temp = lag_dates$hourly_dry_bulb_temperature),
+                    by = list(date = as.POSIXct(format(lag_dates$date,format='%Y-%m-%d')),
+                              name = lag_dates$name),
+                    function(x) {round(mean(x),0)})
 
-air_avg <- c(cp_test_avg, lag_test_avg)
+air_avg <- bind_rows(cp_avg, lag_avg) %>%
+  mutate(date = (ymd(date)))
 
-landsat$air_avg <- air_avg
+shape_temps <- dplyr::left_join(air_avg, raster_shape) %>% 
+  mutate(colum_label = NULL)
+

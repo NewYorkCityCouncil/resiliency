f9227d98886c906ac07f86b6fde7bb706684cd06
Ben Witte <bwitte@council.nyc.gov>
2020-07-01 15:03:56 -0400

    ac/comorb map adjusted to check boxes

--- code/05_covid_heatvuln_mapping.R
+++ code/05_covid_heatvuln_mapping.R
@@ -245,8 +245,9 @@ map2 <- leaflet(options = leafletOptions(zoomControl = FALSE, minZoom = 10, maxZ
   addLegend(position = "topleft", pal = legend_pal, values = legend_val, title = paste0("Temperature Deviation", "<br>", "from Mean"),  labFormat = labelFormat(prefix = " ")) %>% 
   addLegend(position = 'topleft', pal = ac_pal, values = ac_map_sf$Percent.of.Households, group = "Air Conditioning Access", title = "Percent Households with AC")  %>% 
   addLegend(position = "topleft", pal = comorbidity_pal, values = relevant_comorbidities$intersect_prob, group = "Comorbidities", title = paste0("Percent Residents", "<br>", "with Hypertension/COPD")) %>%
-  addLayersControl(baseGroups = c("Heat Map", "Air Conditioning Access", "Comorbidities"),
-                   options = layersControlOptions(collapsed = FALSE), position = "bottomright")
+  addLayersControl(overlayGroups = c("Heat Map", "Air Conditioning Access", "Comorbidities"),
+                   options = layersControlOptions(collapsed = FALSE), position = "bottomright") %>% 
+  hideGroup(c("Air Conditioning Access", "Comorbidities"))
 
 map2
 
